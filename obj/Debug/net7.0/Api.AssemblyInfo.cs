f5f081a5d8285803e2c7754da526ea08a930001e
mcabralportfoliopersonal <mcabral@portfoliopersonal.com>
2024-09-25 19:36:24 -0300

    publish

--- obj/Debug/net7.0/Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a8d7f33dd3c88cf4f9dc19ff9d58b23177b046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08dec7276140adbe43b8718833192f6c996f09b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
