d30f6c542184f84095ace9095f3444926d6e4606
mcabralportfoliopersonal <mcabral@portfoliopersonal.com>
2024-09-25 19:41:29 -0300

    fix

--- obj/Debug/net7.0/Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f081a5d8285803e2c7754da526ea08a930001e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c2f44d98e8a93737e8344f200fbdda443119ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
