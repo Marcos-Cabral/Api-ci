abc2613b651786702c99e357b9a99c1453e31c3f
mcabralportfoliopersonal <mcabral@portfoliopersonal.com>
2024-09-26 09:36:45 -0300

    test

--- Testing/OrdenesServiceTests.cs
+++ Testing/OrdenesServiceTests.cs
@@ -17,7 +17,7 @@ namespace Api.Tests
         [Test]
         public void GetAll_Returns_ListOfOrdenes()
         {
-            List<Orden> result = _ordenesService.GetAll();
+            List<Orden> result = _ordenesService.GetAll(); 
             Assert.IsNotNull(result, "El resultado no debe ser nulo.");
             Assert.IsInstanceOf<List<Orden>>(result, "El resultado debe ser una lista de Orden.");
             Assert.IsTrue(result.Count > 0, "La lista debe contener al menos un elemento.");
